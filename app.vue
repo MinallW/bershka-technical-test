0a9000c6d48e2c16f0814b6d0626a8bd63c98d47
Miguel <suarezmiguelc@icloud.com>
2023-08-05 19:01:01 +0200

    Simple counter to check devOps pipeline

--- app.vue
+++ app.vue
@@ -2,4 +2,16 @@
   <div>
     <NuxtWelcome />
   </div>
+  <button @click="increment">Increment {{count}}</button>
+  <button @click="decrement">Decrement</button>
+
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const increment = () => count.value++
+const decrement = () => count.value--
+</script>
