f425c82154f74e6b11fe3620c9c1b39f23039188
Miguel <suarezmiguelc@icloud.com>
2023-08-09 16:43:04 +0200

    Working on content

--- app.vue
+++ app.vue
@@ -1,3 +1,8 @@
 <template>
-  <NuxtLayout/>
+  <div class="flex">
+    <NuxtLayout />
+    <div class="ml-[100px] flex-1 p-3">
+      <NuxtPage/>
+    </div>
+  </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>Index page</h1>
+    <p>dlkfjsdiufghnsda9gujsoajgffiuodagjhfdughodfgjofdgjoidfjgofdjgoidfjgoifdjogfjdgoifdjogifdjgofidj</p>
+</template>
