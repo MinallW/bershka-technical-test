545f42fcf432545a73edb362321c15c334774810
Miguel <suarezmiguelc@icloud.com>
2023-08-09 21:18:24 +0200

    goToRoute on Select, with options

--- components/Search.vue
+++ components/Search.vue
@@ -1,10 +1,83 @@
 <template>
-    <div class="relative mt-2 rounded-md shadow-sm">
-        <input type="text" name="routes"
-            class="block w-full rounded-md border-0 py-1.5 pl-5 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            placeholder="Qué ropa estás buscando?">
-        <div class="absolute inset-y-0 right-0 flex items-center">
+    <Listbox as="div" v-model="selected">
+        <ListboxLabel class="block text-sm font-medium text-gray-900">Assigned to</ListboxLabel>
+        <div class="relative">
+            <ListboxButton
+                class="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
+                <span class="flex items-center">
+                    <span class="ml-3 block truncate">{{ selected.name }}</span>
+                </span>
+                <span class="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+                    <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+                </span>
 
+            </ListboxButton>
+
+            <transition leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100"
+                leave-to-class="opacity-0">
+                <ListboxOptions
+                    class="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                    <ListboxOption as="template" v-for="route in routes" :key="route.id" :value="route"
+                        v-slot="{ active, selected }">
+                        <li
+                            :class="[active ? 'bg-indigo-600 text-white' : 'text-gray-900', 'relative cursor-default select-none py-2 pl-3 pr-9']">
+                            <div class="flex items-center">
+
+                                <span :class="[selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate']">{{
+                                    route.name }}</span>
+                            </div>
+
+                            <span v-if="selected"
+                                :class="[active ? 'text-white' : 'text-indigo-600', 'absolute inset-y-0 right-0 flex items-center pr-4']">
+                                <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                            </span>
+                            <button @click="goToRoute(route)" class="absolute inset-0 w-full h-full cursor-pointer">
+                            </button>
+                        </li>
+                    </ListboxOption>
+                </ListboxOptions>
+            </transition>
         </div>
-    </div>
+    </Listbox>
 </template>
+  
+<script setup>
+import { ref } from 'vue'
+import { Listbox, ListboxButton, ListboxLabel, ListboxOption, ListboxOptions } from '@headlessui/vue'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/vue/20/solid'
+
+
+function goToRoute({ route }) {
+    navigateTo(route)
+}
+
+const routes = [
+    {
+        id: 1,
+        name: 'Inicio',
+        route: '/'
+    },
+    {
+        id: 2,
+        name: 'Camisas para Mujer',
+        route: '/women/shirts'
+    },
+    {
+        id: 3,
+        name: 'Pantalones para Mujer',
+        route: '/women/pants'
+    },
+    {
+        id: 4,
+        name: 'Camisas para Hombre',
+        route: '/men/shirts'
+    },
+    {
+        id: 5,
+        name: 'Pantalones para Hombre',
+        route: '/men/pants'
+    }
+]
+
+const selected = ref(routes[0])
+</script>
