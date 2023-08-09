9537ba2417c2aea98c73603255ba7e669f7215d1
Miguel <suarezmiguelc@icloud.com>
2023-08-09 21:25:45 +0200

    Small search update

--- components/Search.vue
+++ components/Search.vue
@@ -5,7 +5,7 @@
             <ListboxButton
                 class="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
                 <span class="flex items-center">
-                    <span class="ml-3 block truncate">{{ selected.name }}</span>
+                    <span class="ml-3 block truncate">Qué prenda estás buscando?</span>
                 </span>
                 <span class="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
                     <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
